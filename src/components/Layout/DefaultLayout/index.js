ffff67f3ea9283e31f1b9cdc4d09a65a8153fee6
thiennn76 <thienphung0706@gmail.com>
2022-12-14 17:28:34 +0700

    Css Header: Logo với Search

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
+import styles from './Default.module.scss';
 
 function DefaultLayout({ children }) {
+    const cx = classNames.bind(styles);
+
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
